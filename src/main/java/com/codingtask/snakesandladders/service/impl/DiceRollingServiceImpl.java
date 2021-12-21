b08db3a65222ec7a24f2dcf3bc8f275694bf7261
Vladyslav Kondratenko <vladyslavkondratenko24@gmail.com>
2021-12-21 18:00:54 +0200

    [SLA-0001] Added ability to roll random dice value

--- src/main/java/com/codingtask/snakesandladders/service/impl/DiceRollingServiceImpl.java
+++ src/main/java/com/codingtask/snakesandladders/service/impl/DiceRollingServiceImpl.java
@@ -4,12 +4,20 @@ import com.codingtask.snakesandladders.service.DiceRollingService;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 
 @Service
 public class DiceRollingServiceImpl implements DiceRollingService {
 
+	private static final Random RANDOM = new Random();
+
+	private static final int LEFT_BOUND = 1;
+	private static final int RIGHT_BOUND = 6;
+
+
 	@Override
 	public int roll() {
-		return 0;
+		return LEFT_BOUND + RANDOM.nextInt(RIGHT_BOUND);
 	}
 }
